defcf717760039785d6bd964f67d499a67996885
amalsimon2 <amalsimc@gmail.com>
2026-02-06 21:41:41 +0530

    Task4

---
+++ Patterns/program14.java
@@ -0,0 +1,15 @@
+package Patterns;
+import java.util.Scanner;
+
+public class program14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++)
+                System.out.print((char)('A' + j) + " ");
+            System.out.println();
+        }
+    }
+}
